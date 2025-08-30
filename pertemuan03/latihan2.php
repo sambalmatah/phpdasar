dcc188cdb8c2b4bafb00daa0cccd8269780e5d63
sambalmatah <yudiarinugroho14@gmail.com>
2025-08-30 15:47:04 +0700

    menambahkan pertemuan 3 pengkondisian

--- pertemuan03/latihan2.php
+++ pertemuan03/latihan2.php
@@ -5,19 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 2</title>
 
+    <style>
+        .warna-baris {
+            background-color: silver;
+        }
+    </style>
+
 </head>
 <body>
     <h1>Test Table</h1>
     <table border="1" cellpadding="10" cellspacing="0">
         <?php 
-            for( $i = 1; $i <= 3; $i++ ) {
-                echo "<tr>";
-                for( $j = 1; $j <= 5; $j++ ) {
-                    echo "<td>$i,$j</td>";
-                }
-                echo "</tr>";
-            }
-        ?>
+            for( $i = 1; $i <= 8; $i++ ) : ?>
+                <?php if( $i % 2 == 1) : ?>
+                <tr>
+                <?php else : ?>
+                <tr class="warna-baris">
+                <?php endif; ?>
+                <?php for( $j = 1; $j <= 5; $j++ ) : ?> 
+                    <td>
+                        <?php echo "$i,$j" ?>
+                    </td>
+                <?php endfor; ?>
+                </tr>
+        <?php endfor; ?>
     </table>
 </body>
 </html>
---
+++ pertemuan03/latihan3.php
@@ -0,0 +1,19 @@
+<?php 
+// Pengkondisian / Percabangan
+// if else
+// if elseif else
+// ternary
+// switch
+
+$x = 20;
+if( $x < 20 ) {
+    echo "benar";
+
+} elseif ( $x == 20 ) {
+    echo "bingo!"; 
+
+} else {
+    echo "salah";
+
+}
+?>
